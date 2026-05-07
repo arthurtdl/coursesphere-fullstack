8199452e5fc3d81e4021fd11169725aff0f639f5
Arthur Torres <arthur.torresdl03@gmail.com>
2026-05-06 23:21:07 -0300

    feat(setup-user-crud): Added controller, representer and unit test with rspec

---
+++ server/app/representers/user_representer.rb
@@ -0,0 +1,20 @@
+class UserRepresenter
+  def initialize(user)
+    @user = user
+  end
+
+  # Returns a JSON representation of the user with only the id, name, and email attributes
+  def as_json
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    }
+  end
+
+  private
+
+  # Provides read access to the user instance variable for use in the as_json method
+  attr_reader :user
+
+end
