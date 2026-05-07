f868049c7965bf218aa294158fec5728e48cb099
Arthur Torres <arthur.torresdl03@gmail.com>
2026-05-07 01:20:51 -0300

    feat(setup-lesson-crud): Added controller, representer and rspec with unit test

---
+++ server/app/controllers/api/v1/lessons_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::LessonsController < ApplicationController
+  before_action :set_lesson, only: [:show, :update, :destroy]
+
+  # GET /api/v1/lessons
+  def index
+    lessons = Lesson.includes(:course).all
+    render json: LessonRepresenter.new(lessons).as_json
+  end
+
+  # GET /api/v1/lessons/:id
+  def show
+    render json: LessonRepresenter.new(@lesson).as_json
+  end
+
+  # POST /api/v1/lessons
+  def create
+    @lesson = Lesson.new(lesson_params)
+
+    if @lesson.save
+      render json: LessonRepresenter.new(@lesson).as_json, status: :created
+    else
+      render json: @lesson.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/lessons/:id
+  def update
+    if @lesson.update(lesson_params)
+      render json: LessonRepresenter.new(@lesson).as_json
+    else
+      render json: @lesson.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/lessons/:id
+  def destroy
+    @lesson.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :description, :video_url, :status, :course_id)
+  end
+end
