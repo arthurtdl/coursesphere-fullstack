73c2ac937293d90755f5f8cbbbf9133e5697a630
Arthur Torres <arthur.torresdl03@gmail.com>
2026-05-07 15:33:50 -0300

    feat(add-clientdependencies): Added react-router-dom configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes/index.tsx"
+
 function App() {
   return (
-       <div>
-        Hello World
-       </div>
+    <RouterProvider router={router} />
   )
 }
 
