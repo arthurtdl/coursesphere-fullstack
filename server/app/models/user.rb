ee9a8577f5ae4cff48c7c81608b9b5e989b3fb7a
Arthur Torres <arthur.torresdl03@gmail.com>
2026-05-07 00:30:42 -0300

    feat(setup-courses-crud): Added courses model with rspec unit tests

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  # When a user is deleted, their courses will also be deleted
+  has_many :courses, foreign_key: 'author_id', dependent: :destroy
+
   # Validations
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_07_001735) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_07_031052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "start_date", null: false
+    t.datetime "end_date", null: false
+    t.integer "status", default: 0, null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_courses_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_07_001735) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "courses", "users", column: "author_id"
 end
