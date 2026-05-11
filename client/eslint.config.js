19b9584f97668bd764826e89842b811fdcd09e93
Arthur Torres <arthur.torresdl03@gmail.com>
2026-05-11 11:09:54 -0300

    refactor(add-lessons-screen): Refactored eslint.config.js to allow constant export

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -11,12 +11,21 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 ])
